f187f04090145100c5b9d3c1fef501e6014787fe
fernandes-draw <fernandes_draw@hotmail.com>
2018-12-01 21:01:56 -0200

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println(new Position(3, 5));
+		
+	}
+	
+}
